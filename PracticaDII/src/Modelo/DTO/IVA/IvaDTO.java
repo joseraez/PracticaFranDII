93ab8aa59ee877141a28e7457095d2b04ca235d9
José Ráez Rodríguez <joseraezrodriguez@gmail.com>
2016-11-10 13:12:29 +0100

    Add files via upload

---
+++ PracticaDII/src/Modelo/DTO/IVA/IvaDTO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.DTO.IVA;
+
+/**
+ *
+ * @author Administrador
+ */
+public class IvaDTO {
+    
+}
